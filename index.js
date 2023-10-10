bf58d883408d9672ec94f34326d61b89062ebd78
MrTanguy <tanguy.meignier@gmail.com>
2023-10-11 00:49:53 +0200

    Implémentation API, création calendrier

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
-module.exports = getAllDaysOfMonth;
+const getAllDaysOfMonth = (month, year) => {
 
-function getAllDaysOfMonth (month, year) {
+    // Récupère le jour du 1er : 0 = dimanche
+    let firstDay = new Date(year, month, 1).getDay()
 
-    let nextMonth = new Date(year, month + 1, 1);
+    // Récupère le dernier jour du mois 
+    let totalDay = new Date(year, month+1, 0).getDate()
 
-    let lastDayOfMonth = new Date(nextMonth - 1);
+    let tableHtml = `<table>  
+                        <thead> 
+                            <tr> 
+                                <th>Sun</th> 
+                                <th>Mon</th> 
+                                <th>Tue</th> 
+                                <th>Wed</th> 
+                                <th>Thu</th> 
+                                <th>Fri</th> 
+                                <th>Sat</th>    
+                            </tr> 
+                        </thead>
+                        <tbody >
+                            <tr>`;
 
-    let days = [];
+    for (let i = 1 ; i <= totalDay + firstDay ; i++) {
 
-    for (let day = 1; day <= lastDayOfMonth.getDate(); day++) {
-        days.push(new Date(year, month, day));
+        if (i - 1 >= firstDay) {
+            tableHtml += `<td>${i - firstDay}</td>`;
+        } else {
+            tableHtml += '<td></td>';
+        }
+
+        if (i % 7 === 0) {
+            tableHtml += '</tr>';
+        } 
+        
     }
 
-    console.log(days)
+    tableHtml += '</tbody></table>';
 
-    return days;
+    return tableHtml;
 }
+
+module.exports = getAllDaysOfMonth;
